b147e5767a5b5c875da5511ac82e6ff3c3457add
Thanh <thanh.nc2803@gmail.com>
2020-06-14 13:02:45 +0900

    Hoc phep gan

---
+++ phep-gan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+// Phép gán
+int main()
+{
+    int a = 10;
+    int b = 5;
+    int c = 2;
+    a = 20;
+    int sum;
+    sum = a + b;
+    cout << sum << endl;
+    sum = sum + c;
+    cout << sum << endl;
+    return 0;
+}
