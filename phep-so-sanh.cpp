c9637a90d17917bea6ac7aadd000370deffc3beb
Thanh <thanh.nc2803@gmail.com>
2020-06-14 12:55:24 +0900

    Hoc phep so sanh

---
+++ phep-so-sanh.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+// Phép so sánh: > >= < <= == !=
+// 0: Fasle
+// 1: True
+int main()
+{
+    int a = 10;
+    int b = 5;
+    cout << "a > b: " << (a > b) << endl;
+    cout << "a >= b: " << (a >= b) << endl;
+    cout << "a < b: " << (a < b) << endl;
+    cout << "a <= b: " << (a <= b) << endl;
+    cout << "a == b: " << (a == b) << endl;
+    cout << "a != b: " << (a != b) << endl;
+
+    return 0;
+}
